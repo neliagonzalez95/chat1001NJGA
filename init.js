871f2db7b15c2f8ab933fcfb90f1f9c14afd85b7
neliagonzalez95 <93960399+neliagonzalez95@users.noreply.github.com>
2021-11-11 01:31:18 -0600

    Create init.js

---
+++ init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({	
+  apiKey: "AIzaSyBiY9LB8q0H3bejHtKnFHOBCkzfIGfiexk",
+  authDomain: "practica-2-a16c9.firebaseapp.com",
+  projectId: "practica-2-a16c9",
+  storageBucket: "practica-2-a16c9.appspot.com",
+  messagingSenderId: "434391747272",
+  appId: "1:434391747272:web:bebebf80f82238deb8f3fd",
+  measurementId: "G-BBG6Z4ZM48"
+    });
